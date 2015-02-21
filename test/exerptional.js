6d1b472e805a77ab1a5953d33c0af25402419ca7
Kristofer Palmvik <info@kristofer.pp.se>
2015-02-21 15:40:15 +0100

    Added promise based tests for centers

--- test/exerptional.js
+++ test/exerptional.js
@@ -1,10 +1,16 @@
 define([
     'intern!bdd',
     'intern/chai!expect',
+    'intern/chai!',
+    'intern/dojo/node!chai-as-promised',
     'intern/dojo/node!../exerptional' // Load as a NPM CommonJS module
-], function (bdd, expect, exerptional) {
+], function (bdd, expect, chai, chaiAsPromised, exerptional) {
       var describe = bdd.describe,
-          it = bdd.it;
+          it = bdd.it,
+          beforeEach = bdd.beforeEach;
+
+      chai.use(chaiAsPromised);
+      chai.should();
 
       describe('Sanity check', function() {
         var createClient = exerptional.createClient;
@@ -26,5 +32,23 @@ define([
                 .to.throw(Error, 'A base URL must be defined!');
           }
         );
+    });
+
+    describe('List centers', function() {
+      var createClient = exerptional.createClient,
+          dummyUrl = 'http://dummy.url/',
+          client;
+
+      beforeEach(function () {
+          client = createClient(dummyUrl);
+      });
+
+      it('should return an array eventually', function() {
+        return client.allCenters().should.eventually.be.an.array;
+      });
+
+      it('should have at least one center in the array', function() {
+        return client.allCenters().should.eventually.not.be.empty;
+      });
     });
 });
