1dd511ae443d60f55a73f44dc390007cbee25216
Kristofer Palmvik <info@kristofer.pp.se>
2015-02-21 15:04:53 +0100

    Changed tests to use BDD style

--- test/exerptional.js
+++ test/exerptional.js
@@ -1,24 +1,30 @@
 define([
-    'intern!object',
+    'intern!bdd',
     'intern/chai!expect',
     'intern/dojo/node!../exerptional' // Load as a NPM CommonJS module
-], function (registerSuite, expect, exerptional) {
-  registerSuite({
-          name: 'exerptional',
+], function (bdd, expect, exerptional) {
+      var describe = bdd.describe,
+          it = bdd.it;
 
-          createClient: function () {
-            var createClient = exerptional.createClient;
-            var dummyBaseUrl = 'http://dummy.url/';
+      describe('Sanity check', function() {
+        var createClient = exerptional.createClient;
 
-            expect(
-              createClient(dummyBaseUrl),
-              'Create a client with a specified base URL as required')
-                .to.be.OK;
+        it('should successfully create a client', function () {
+          var dummyBaseUrl = 'http://dummy.url/';
 
+          expect(
+            createClient(dummyBaseUrl),
+            'Create a client with a specified base URL as required')
+              .to.be.OK;
+          }
+        );
+
+        it('should thrown an error for a client without a specified base URL', function () {
             expect(
               createClient,
-              'Create a client without a specified base URL should result in an error')
+              'Create a should result in an error')
                 .to.throw(Error, 'A base URL must be defined!');
           }
-      });
+        );
+    });
 });
